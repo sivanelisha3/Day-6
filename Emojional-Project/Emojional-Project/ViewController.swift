6ddbc9c11a6f4192fe451704285955a97559f4bd
Sivan <sivan@Apples-MacBook-Pro-62.local>
2019-08-12 10:18:56 -0400

    Day-6

---
+++ Emojional-Project/Emojional-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional-Project
+//
+//  Created by Sivan on 8/12/19.
+//  Copyright © 2019 Sivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
