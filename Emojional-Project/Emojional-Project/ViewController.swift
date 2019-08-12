c53ea538046b2560154c312906cc76db3245ea68
Sivan <sivan@Apples-MacBook-Pro-62.local>
2019-08-12 11:53:29 -0400

    Completed Iteration 2

--- Emojional-Project/Emojional-Project/ViewController.swift
+++ Emojional-Project/Emojional-Project/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let emojis = ["😔":"discouraged", "😡":"frustrated"]
+    
+    let customMessages = [
+        "discouraged": ["coding is hard! Keep trying and you'll get it", "Believe in yourself! You got this","It's okay to get discouraged sometimes, but dont let it get in the way of achieving your goals!"],
+        "frustrated": ["Don't be afraid to ask for help!", "Take a deep breath, take a break, and come back to the code later.", "You're doing great! Keep trying and you'll get it!"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showMessage(sender: UIButton){
+        
+        if let selectedEmotion = sender.titleLabel?.text {
+        
+        let titleText = selectedEmotion
+        let messageText = emojis["\(selectedEmotion)"]!
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion]!]?[0]
+        
+        let alertController = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
+        
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        }
+        }
 
 }
+
+
 
